5002681eb8f621d0bae33d7ee2e028ac6ac32b81
Stefanus <z3442577@zmail.unsw.edu.au>
2013-10-01 16:42:31 +1000

    finished subset 1 and 2

--- perl2python.pl
+++ perl2python.pl
@@ -5,6 +5,7 @@
 # http://cgi.cse.unsw.edu.au/~cs2041/13s2/assignments/perl2python
 
 while ($line = <>) {
+
 	if ($line =~ /^#!/ && $. == 1) {
 	
 		# translate #! line 
@@ -18,9 +19,48 @@ while ($line = <>) {
 	} elsif ($line =~ /^\s*print\s*"(.*)\\n"[\s;]*$/) {
 		# Python's print adds a new-line character by default
 		# so we need to delete it from the Perl print statement
+		# delete the dollar sign infront of variable after print statement.
+		# remove double quote from enclosing the variable. 
+		# at the same time by not using $1, and change it to s to keep indentation.
+
+		$line =~ s/"|\\n|\$|;//g;
+		print "$line";
 		
-		print "print \"$1\"\n";
-	} else {
+		
+	} elsif ($line =~ /\$/){
+
+		#to remove all variable with $ infront of them and remove 
+		#the semi colon after that.
+		$line =~ s/\$//g;
+		$line =~ s/;//g;
+
+		#to remove things with a comma and semi collon inside it 
+		#and anchor it at the last part of the sentence to match new line in a double colon
+			if($line =~ /, ".*"/){
+				$line =~ s/, "\\n"$//;
+			}  
+
+			if ($line =~ /if/ || $line =~ /while/){
+				
+				$line =~ s/(\)|\()//g;
+				$line =~ s/{|\}|\{|}//g;
+				$line =~ s/ \n/:\n/;
+
+			}
+
+		print $line;
+
+	} elsif ($line =~ /^if/ || $line =~ /^while/){
+		
+				$line =~ s/(\)|\()//g;
+				$line =~ s/{|\}|\{|}//g;
+				$line =~ s/ \n/:\n/;
+
+				print $line;
+
+	}elsif($line =~ /}/){
+			$line =~ s/}//g;
+	}else {
 	
 		# Lines we can't translate are turned into comments
 		
